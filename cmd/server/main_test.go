798466cdbf5bbeebbcef6c84f09a89ef5f14fea9
Alexandre <alr-lab@users.noreply.github.com>
2021-03-03 23:03:17 +0100

    Add UI test example (#22)
    * Add UI test example
    
    * Add documentation example

---
+++ cmd/server/main_test.go
@@ -0,0 +1,39 @@
+package main_test
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
+
+func Test(t *testing.T) {
+	// Arrange
+	req, err := http.NewRequest("GET", "http://app:8080/", nil)
+	if err != nil {
+		t.Fatalf("Unable to create application request, err = %s", err)
+	}
+
+	// Act
+	res, err := (http.DefaultClient).Do(req)
+
+	// Assert
+	if err != nil {
+		t.Fatalf("Unable to request application, err = %s", err)
+	}
+	defer res.Body.Close()
+
+	raw, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Unable to read response, err = %s", err)
+	}
+
+	var decoded response
+	if err := json.Unmarshal(raw, &decoded); err != nil {
+		t.Fatalf("Unable to decode response, err = %s", err)
+	}
+}
