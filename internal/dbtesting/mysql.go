646c7922220c6b5a1b4260b3d54b6bfd00393219
Alexandre Le Roy <ale-roy@student.42.fr>
2021-03-26 20:46:15 +0100

    Refactor internaldb

--- internal/dbtesting/mysql.go
+++ internal/dbtesting/mysql.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Alexandre Le Roy. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in
+// the LICENSE file.
+
 package dbtesting
 
 import (
@@ -10,13 +14,18 @@ import (
 	"testing"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql" // needed to use MySQL
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
 	"gopkg.in/testfixtures.v2"
 )
 
 const (
+	// Testing database hostname
 	hostname = "datastore"
+
+	// Testing database username
 	username = "root"
+
+	// Testing database password
 	password = "root"
 )
 
@@ -28,9 +37,10 @@ func DatabaseHelper(t *testing.T, fixtureDir string) *sql.DB {
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
 	if err != nil {
-		t.Fatalf("Unable to generate uuid")
+		t.Fatalf("Unable to generate database schema UUID")
 	}
 
+	// Database schema, must start with a letter
 	schema := fmt.Sprintf("a_%x_%x_%x_%x_%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 
 	dsn := fmt.Sprintf(
@@ -45,9 +55,8 @@ func DatabaseHelper(t *testing.T, fixtureDir string) *sql.DB {
 	if err != nil {
 		t.Fatalf("Unable to open local MySQL connection, err = %s", err)
 	}
-
-	conn.SetMaxOpenConns(5)
-	conn.SetConnMaxLifetime(10 * time.Minute)
+	conn.SetMaxOpenConns(1)
+	conn.SetConnMaxLifetime(2 * time.Minute)
 
 	query := fmt.Sprintf(
 		`SET GLOBAL max_connections = 10; DROP DATABASE IF EXISTS %[1]s; CREATE DATABASE %[1]s; USE %[1]s;`,
@@ -71,16 +80,23 @@ func DatabaseHelper(t *testing.T, fixtureDir string) *sql.DB {
 	}
 
 	if fixtureDir != "" {
-		testfixtures.SkipDatabaseNameCheck(true)
-		fixtures, err := testfixtures.NewFolder(conn, &testfixtures.MySQL{}, fixtureDir)
-		if err != nil {
-			t.Fatalf("Unable to create fixtures from fixture directory, err = %s", err)
-		}
-
-		if err := fixtures.Load(); err != nil {
-			t.Fatalf("Unable to load fixtures, err = %s", err)
-		}
+		fixtures(t, conn, fixtureDir)
 	}
 
 	return conn
+}
+
+// Run fixtures against newly created database described by the connection
+// given as an argument
+func fixtures(t *testing.T, conn *sql.DB, fixtureDir string) {
+	testfixtures.SkipDatabaseNameCheck(true)
+
+	fixtures, err := testfixtures.NewFolder(conn, &testfixtures.MySQL{}, fixtureDir)
+	if err != nil {
+		t.Fatalf("Unable to create fixtures from fixture directory, err = %s", err)
+	}
+
+	if err := fixtures.Load(); err != nil {
+		t.Fatalf("Unable to load fixtures, err = %s", err)
+	}
 }
